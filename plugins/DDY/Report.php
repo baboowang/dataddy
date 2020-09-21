35f0b98ad77f28677c6b6d5829da447fdea78437
baboo <baboo.wg@gmail.com>
2020-09-21 14:29:53 +0800

    feat: DDY default report plugin

---
+++ plugins/DDY/Report.php
@@ -0,0 +1,41 @@
+<?php
+namespace PL\DDY;
+
+class Report extends \MY\Plugin_Abstract
+{
+    public function pluginInit($dispatcher, $manager)
+    {
+        \MY\Data_Template::registerPlugin($this);
+    }
+
+    public function field_percent($config_value, $value, $field, $i, $row, $report)
+    {
+        if (!is_array($config_value)) {
+            $config_value = [
+                'base' => $config_value,
+            ];
+        }
+        if (!isset($config_value['succ'])) {
+            $config_value['succ'] = 70;
+        }
+        if (preg_match('@^(.+)?\.(.+)$@u', $config_value['base'], $ma)) {
+            $base = $report['rows'][$ma[1]][$ma[2]];
+        } else {
+            $base = $row[$config_value['base']];
+        }
+
+        if ($base && is_numeric($base)) {
+            $percent = round($value / $base * 100, $config_value['dot'] ?? 0);
+        } else {
+            $percent = '-';
+        }
+        $class = 'badge badge-default';
+        if (isset($config_value['succ']) && $percent >= $config_value['succ']) {
+            $class = 'badge badge-success';
+        }
+        if ($percent !== '-') {
+            $percent .= '%';
+        }
+        return "{$value} <span class='{$class}'>{$percent}</span>";
+    }
+}
